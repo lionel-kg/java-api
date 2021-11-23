43f3b369730af7d50dee57b6468f16d71cf7fe97
lionel-kg <lionel77350@gmail.com>
2021-11-23 12:05:46 +0100

    methods getProductById

--- src/main/java/com/example/ynov/productapi/controller/ProductController.java
+++ src/main/java/com/example/ynov/productapi/controller/ProductController.java
@@ -1,7 +1,10 @@
 package com.example.ynov.productapi.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ynov.productapi.model.Product;
@@ -16,6 +19,11 @@ public class ProductController {
 	@GetMapping("/product")
 	public Iterable<Product> getProducts() {
 		return productRepository.findAll();
+	}
+	
+	@GetMapping("/product/{id}")
+	public Optional<Product> getProduct(@PathVariable("id") Integer id) {
+		return productRepository.findById(id);
 	}
 
 }
